b795b3bb1c90df871cd1bd68355ccbd24f16e642
Astrian Zheng <git@astrianzheng.cn>
2021-09-10 20:43:15 +0800

    Initial Commit

---
+++ star/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  star
+//
+//  Created by Astrian Zheng on 2021/9/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ star/starApp.swift
@@ -0,0 +1,17 @@
+//
+//  starApp.swift
+//  star
+//
+//  Created by Astrian Zheng on 2021/9/10.
+//
+
+import SwiftUI
+
+@main
+struct starApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
