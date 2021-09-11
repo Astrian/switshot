1cdb96d577c02a3ac98ce5082dcfff4f93a53107
Astrian Zheng <git@astrianzheng.cn>
2021-09-11 12:41:21 +0800

    About view

--- star/Views/AboutView.swift
+++ star/Views/AboutView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct AboutView: View {
+  @State private var appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+  
   var body: some View {
-    Form {
-      HStack {
-        Text("Version")
-        Spacer()
-        Text("a")
-          .foregroundColor(Color.gray)
+    NavigationView {
+      Form {
+        HStack {
+          Text("AboutView_Version")
+          Spacer()
+          Text(appVersion ?? "?")
+            .foregroundColor(Color.gray)
+        }
+        
+        Section (footer: Text("AboutView_OpenSource")) {
+          Button(action: {
+            guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+              return
+            }
+            UIApplication.shared.open(settingsUrl)
+          }) {
+            Text("AboutView_SystemSettings")
+          }
+          Button(action: {
+            guard let settingsUrl = URL(string: "https://github.com/Astrian/switshoot") else {
+              return
+            }
+            UIApplication.shared.open(settingsUrl)
+          }) {
+            Text("AboutView_Repo")
+          }
+        }
       }
+      .navigationTitle("AboutView_Title")
     }
   }
 }
