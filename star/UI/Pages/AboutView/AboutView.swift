da89444307f2c95c62a4360f115575e1ad1e355f
Astrian Zheng <git@astrianzheng.cn>
2021-12-05 21:31:42 +0800

    Update information in Help page

--- star/UI/Pages/AboutView/AboutView.swift
+++ star/UI/Pages/AboutView/AboutView.swift
@@ -20,13 +20,37 @@ struct AboutView: View {
             .foregroundColor(Color.gray)
         }
         
-        Section (footer: Text("AboutView_OpenSource")) {
+        Section {
           Link("AboutView_SystemSettings", destination: URL(string: UIApplication.openSettingsURLString)!)
+        }
+        
+        Section {
+          Link("AboutView_HelpCenter", destination: URL(string: NSLocalizedString("AboutView_HelpCenter_Link", comment: ""))!)
+          Link("AboutView_Updates", destination: URL(string: "https://updates.switshot.app/")!)
+        }
+        
+        Section (header: Text("AboutView_Appreciation")) {
+          VStack (alignment: .leading){
+            Text("AboutView_Appreciation_Contributor_EanaHufwe").bold()
+            Text("AboutView_Appreciation_ContibuteTitle_EanaHufwe").font(.callout)
+            Link("AboutView_Appreciation_Contibutor_Website", destination: URL(string: "https://1a23.com/")!).font(.callout)
+          }
+        }
+        
+        Section (footer: Text("AboutView_OpenSource")) {
           Link("AboutView_Repo", destination: URL(string: "https://github.com/Astrian/switshot")!)
         }
       }
       .navigationTitle("AboutView_Title")
-      
+      .toolbar {
+        ToolbarItem(placement: .navigationBarLeading) {
+          Button {
+            presented = false
+          } label: {
+            Text("AboutView_Dismiss")
+          }
+        }
+      }
     }
   }
 }
