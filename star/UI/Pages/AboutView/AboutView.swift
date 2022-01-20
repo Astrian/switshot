604b26efd3d155bad02e8a8a42f77f18614ac6d6
Astrian Zheng <git@astrianzheng.cn>
2022-01-20 19:17:55 +0800

    Add weibo link

--- star/UI/Pages/AboutView/AboutView.swift
+++ star/UI/Pages/AboutView/AboutView.swift
@@ -27,10 +27,14 @@ struct AboutView: View {
         Section {
           Link("AboutView_HelpCenter", destination: URL(string: NSLocalizedString("AboutView_HelpCenter_Link", comment: ""))!)
           Link("AboutView_Updates", destination: URL(string: "https://updates.switshot.app/")!)
-          Link("AboutView_Discord", destination: URL(string: "https://discord.gg/zeT2nnGvAA")!)
         }
         
-        Section (header: Text("AboutView_Appreciation")) {
+        Section(header: Text("AboutView_Comm")) {
+          Link("AboutView_Discord", destination: URL(string: "https://discord.gg/zeT2nnGvAA")!)
+          Link("AboutView_QQ", destination: URL(string: "https://jq.qq.com/?_wv=1027&k=VFMbQrFe")!)
+        }
+        
+        Section(header: Text("AboutView_Appreciation")) {
           VStack (alignment: .leading){
             Text("AboutView_Appreciation_Contributor_EanaHufwe").bold()
             Text("AboutView_Appreciation_ContibuteTitle_EanaHufwe").font(.callout)
@@ -39,6 +43,7 @@ struct AboutView: View {
         }
         
         Section (footer: Text("AboutView_OpenSource")) {
+          Link("AboutView_Weibo", destination: URL(string: "https://weibo.com/n/Narixtas")!)
           Link("AboutView_Repo", destination: URL(string: "https://github.com/Astrian/switshot")!)
         }
       }
