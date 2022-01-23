fa75489b3eded084a1bd300f4a0367e311b11c96
Astrian Zheng <git@astrianzheng.cn>
2022-01-23 22:03:28 +0800

    Add new App Clip target

---
+++ lucifer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lucifer
+//
+//  Created by Astrian Zheng on 2022/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
